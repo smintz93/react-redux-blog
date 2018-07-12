59fd8a3f4b120553583f2c99a92cc67d310590a9
Sam <sammintz1993@gmail.com>
2018-07-12 12:00:16 -0500

    axios api call

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,15 @@
+import axios form 'axios';
+
 export const FETCH_POSTS = 'fetch_posts'
 
+const ROOT_URL = 'http://reduxblog.herokuapp.com/api/'
+const API_KEY = '?key=SOCCERBALL1234'
+
 export function fetchPosts(){
+	const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
+
 	return {
-		type: FETCH_POSTS
+		type: FETCH_POSTS,
+		payload: request
 	};
 }
