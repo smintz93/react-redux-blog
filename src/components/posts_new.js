a40b44d3fba67d2c56f7dd6ee14db3381bba1102
Sam <sammintz1993@gmail.com>
2018-07-20 11:21:30 -0500

    added validate() to validate form and check for erros

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -22,15 +22,19 @@ class PostsNew extends Component {
 		return (
 			<form>
 				<Field 
-					label="Title"
+					label="Title For Post"
 					name="title"
 					// supposed to be a function that returns jsx
 					component={this.renderField}
 				/>
 				<Field 
-					label="Tags"
-					name="tags"
-					// supposed to be a function that returns jsx
+					label="Categories"
+					name="Categories"
+					component={this.renderField}
+				/>
+				<Field 
+					label="Post Content"
+					name="Content"
 					component={this.renderField}
 				/>
 			</form>	
@@ -38,6 +42,20 @@ class PostsNew extends Component {
 	}
 }
 
+// values is what user enters into the form
+validate = (values) => {
+	// need to return an object
+	const errors = {} 
+	// validate inputs from values
+
+
+	// if errors is empty form is fine to submit.
+	// if erros has any props, redux form assumes form is invalid
+	return errors; 	
+}
+
 export default reduxForm({
+	validate,
 	form: 'PostsNewForm' 	// pass a function helper (just like connect) // unique string
 })(PostsNew); 
+
