52a24593af9e2e68b2307026245fbd586bd966dc
Sam <sammintz1993@gmail.com>
2018-07-20 11:13:37 -0500

    refactored code to make dry by changing renderfield function

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -3,11 +3,13 @@ import { Field, reduxForm } from 'redux-form';
 // reduxForm allowing component to talk to redux store
 
 class PostsNew extends Component {
-	renderTitleField = (field) => {
+	renderField = (field) => {
 		// so field knows its responsible for text input
 		return (
-			<div>
-				<input 
+			<div className="form-group">
+				<label>{field.label}:</label>
+				 <input 
+				 	className="form-control" 
 					type="text"
 					// field.input is an object which contains object, props, and value
 					{...field.input}
@@ -20,9 +22,16 @@ class PostsNew extends Component {
 		return (
 			<form>
 				<Field 
+					label="Title"
 					name="title"
 					// supposed to be a function that returns jsx
-					component={this.renderTitleField}
+					component={this.renderField}
+				/>
+				<Field 
+					label="Tags"
+					name="tags"
+					// supposed to be a function that returns jsx
+					component={this.renderField}
 				/>
 			</form>	
 		)
