1b3aff9a39d54928b32aa33609fcab60bdf414db
Sam <sammintz1993@gmail.com>
2018-07-25 13:10:31 -0500

    new action creator for navagating to just one single post

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,8 +1,17 @@
-import { FETCH_POSTS } from '../actions';
+import { FETCH_POSTS, FETCH_POST  } from '../actions';
 import _ from 'lodash';
 
 export default function(state = {} , action){
 	switch (action.type) {
+	case FETCH_POST:
+		// const post = action.payload.data
+		// // es5
+		// // const newState = { ...state,  };	
+		// // newState[post.id] = post;
+		// // return newState;
+		// es6
+		return { ...state, [action.payload.data.id]: action.payload.data }
+
 	case FETCH_POSTS:
 	// console.log(action.payload.data); //[post1, post2]
 	// // can you lodash .mapKeys
