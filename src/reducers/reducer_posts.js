870ee63412d6090acb025c7e9bd2a3ec8c650ce4
Sam <sammintz1993@gmail.com>
2018-07-31 10:19:37 -0500

    added delete_post reducer case

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,8 +1,11 @@
-import { FETCH_POSTS, FETCH_POST  } from '../actions';
+import { FETCH_POSTS, FETCH_POST, DELETE_POST  } from '../actions';
 import _ from 'lodash';
 
 export default function(state = {} , action){
 	switch (action.type) {
+	case DELETE_POST:
+	// look at state object. if it has key of post id...just drop it and return new state object 
+	return _.omit(state, action.payload);	
 	case FETCH_POST:
 		// const post = action.payload.data
 		// // es5
